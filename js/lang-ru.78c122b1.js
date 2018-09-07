f00bd98e17fbf997827b8613e8a6318408edb013
Лариса  Ангальд <langald@kopilka.kz>
2018-09-07 11:40:22 +0600

    20180907.1

---
+++ js/lang-ru.78c122b1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[1],{zfL3:function(n,e,w){"use strict";w.r(e),e["default"]={greeting:"Привет",name:"Джимми"}}}]);
+//# sourceMappingURL=lang-ru.78c122b1.js.map
